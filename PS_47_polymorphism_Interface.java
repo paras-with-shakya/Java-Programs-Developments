834673303b68edd2dc50fe864fc9fac754cf7e9a
paras-with-shakya <parasshakya67@gmail.com>
2025-09-18 00:05:19 +0530

    all code commit

--- PS_47_polymorphism_Interface.java
+++ PS_47_polymorphism_Interface.java
@@ -4,19 +4,19 @@ class MathOperations {
         return a + b;
     }
 
-    // Overloaded method with three int parameters
-    int add(int a, int b, int c) {
-        return a + b + c;
-    }
-
     // Overloaded method with double parameters
     double add(double a, double b) {
         return a + b;
     }
+
+    // Overloaded method with three int parameters
+    int add(int a, int b, int c) {
+        return a + b + c;
+    }
 }
 
 // Main class
-public class polymorphism {
+public class PS_47_polymorphism_Interface {
     public static void main(String[] args) {
         MathOperations math = new MathOperations();
         System.out.println(math.add(5, 10)); // Output: 15
