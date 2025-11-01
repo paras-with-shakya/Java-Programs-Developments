0321eeb97c198ab16451200f608091f1bf1208fc
paras-with-shakya <parasshakya67@gmail.com>
2025-11-01 08:09:42 +0530

    uptade

--- PS_50_exe11.java
+++ PS_50_exe11.java
@@ -46,12 +46,12 @@ class Human extends Monkey implements BasicAnimal {
 
     @Override
     public void eat() {
-        System.out.println("Human eats");
+        System.out.println("Human eating");
     }
 
     @Override
     public void sleep() {
-        System.out.println("Human sleeps");
+        System.out.println("Human sleeping");
     }
 
 }
@@ -61,15 +61,26 @@ public class PS_50_exe11 {
         // Problem 1 + 2
         // FountainPen fp = new FountainPen();
         // fp.write();
-        // fp.refill();
+        // fp.refill();  
         // fp.ChangNiv();
 
         // Problem 3
-        Human hm = new Human();
-        hm.jump();
-        hm.bite();
-        hm.eat();
-        hm.speak();
+        // Human hm = new Human();
+        // hm.sleep();
+        // hm.jump();
+        // hm.bite();
+        // hm.eat();
+        // hm.speak();
 
+        //Question 5
+        Monkey mn =new Human();
+        mn.jump();
+        mn.bite();
+        // mn.sleep(); // monkey which does not have speak methods This line will cause a compile-time error
+
+        BasicAnimal mess = new Human();
+        // mess.speak();--> //Does not work because BasicAnimal interface does not have speak method
+        mess.eat();
+        mess.sleep();
     }
 }
